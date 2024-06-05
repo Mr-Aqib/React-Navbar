4889c3c005587688e2932463733dc78183c8a9f6
Aqib1422 <mraqibshafique@gmail.com>
2024-06-05 21:12:48 +0500

    navbar

---
+++ src/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Container = () => {
+    return (
+        <div className='m-0 p-0 main-con container-fluid'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Container
